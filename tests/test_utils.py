faacd769c070c67c569c05618b32fdd55d3ec3cd
Sam <Abdusamadov55@gmail.com>
2023-03-12 23:41:15 +0900

    test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,6 @@
 import pytest
 from utils import get_data, get_last_data, get_formatted_data, get_filtered_data
 
-
 def test_get_data():
     url = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678630161075&signature=zulZ1Ikga0shV1j3HZjhS7FQyxa5uRvXTQhCkk8tF2U&downloadName=operations.json"
     assert get_data(url) is not None
